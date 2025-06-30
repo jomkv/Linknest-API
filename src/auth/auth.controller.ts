15ab3ccf4f58ab34d6d7e11b302efd7d6eecace7
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-07-01 00:15:19 +0800

    feat: auth redirect with cookie containing JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get, HttpException, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { type Profile } from 'passport-google-oauth20';
 import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get('auth/google')
   @UseGuards(AuthGuard('google'))
@@ -13,15 +25,28 @@ export class AuthController {
 
   @Get('auth/google/redirect')
   @UseGuards(AuthGuard('google'))
-  async googleHandleRedirect(@Req() req) {
-    const userPayload: Profile | undefined | null = req.user;
+  async googleHandleRedirect(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const userPayload = req.user as Profile | undefined | null;
 
-    if (!userPayload)
+    if (!userPayload) {
       throw new HttpException(
         'Something went wrong, please try again later.',
         500,
       );
+    }
 
-    return await this.authService.authenticate(userPayload);
+    const { accessToken } = await this.authService.authenticate(userPayload);
+
+    res.cookie('access_token', accessToken, {
+      httpOnly: true,
+      secure: this.configService.get<string>('NODE_ENV') === 'production',
+      sameSite: 'strict',
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+
+    return res.redirect(this.configService.get<string>('CLIENT_URL'));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   //app.set('query parser', 'extended');
 
   app.setGlobalPrefix('api', { exclude: ['/api/auth/{*path}'] });
+  app.use(cookieParser());
 
   await app.listen(4040);
 }
