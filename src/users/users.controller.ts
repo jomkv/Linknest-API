b81ad1b785d9f9fc4b92ccd591d277639a701087
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-07-12 18:31:48 +0800

    feat: get current user's links

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,35 @@
-import { Controller, Get, HttpException, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from 'generated/prisma';
 import { LinksService } from 'src/links/links.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { RequestService } from 'src/common/services/request.service';
 
 @Controller('user')
 export class UsersController {
   constructor(
     private readonly usersService: UsersService,
     private readonly linksService: LinksService,
+    private readonly requestService: RequestService,
   ) {}
+
+  @UseGuards(AuthGuard)
+  @Get('links')
+  async getUserLinks() {
+    const user = this.requestService.getUserPayload();
+
+    const links = await this.linksService.getUserLinks(Number(user.sub));
+    return {
+      message: "User's links fetched",
+      data: links,
+    };
+  }
 
   @Get(':displayName')
   async getProfile(@Param('displayName') displayName: string) {
