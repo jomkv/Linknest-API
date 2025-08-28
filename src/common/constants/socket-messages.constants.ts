75f33e47f50198a552306742c9d65d7025c08407
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-08-28 23:29:38 +0800

    feat: session connect and disconnect functionality

---
+++ src/common/constants/socket-messages.constants.ts
@@ -0,0 +1,10 @@
+export const WS_SESSION_MESSAGE = {
+  REGISTER: 'session:register',
+  REGISTERED: 'session:registered',
+};
+
+export const WS_AUTH_MESSAGE = {
+  COMPLETE: 'auth:complete',
+  FAILED: 'auth:failed',
+  LOGOUT: 'auth:logout',
+};
--- src/events/event.gateway.ts
+++ src/events/event.gateway.ts
@@ -1,10 +1,14 @@
 import {
+  ConnectedSocket,
+  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
+import { WS_SESSION_MESSAGE } from 'src/common/constants/socket-messages.constants';
 
 @WebSocketGateway({
   transports: ['websocket'],
@@ -17,13 +21,48 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
-  private clients: Map<string, string> = new Map(); // socketId -> userId
+  private sessionMap: Map<string, string> = new Map(); // socketId -> sessionId
+  private reverseSessionMap: Map<string, string> = new Map(); // sessionId -> socketId
 
-  handleConnection(client: Socket) {
+  handleConnection(socket: Socket) {
     // * TODO
+    // Map user and socketId together if possible
   }
 
-  handleDisconnect(client: Socket) {
-    // * TODO
+  handleDisconnect(socket: Socket) {
+    const sessionId = this.sessionMap.get(socket.id);
+
+    if (sessionId) {
+      this.cleanupSession(sessionId);
+    }
+  }
+
+  @SubscribeMessage(WS_SESSION_MESSAGE.REGISTER)
+  handleSessionRegister(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody('sessionId') sessionId: string,
+  ) {
+    this.sessionMap.set(socket.id, sessionId);
+    this.reverseSessionMap.set(sessionId, socket.id);
+
+    socket.emit(WS_SESSION_MESSAGE.REGISTERED, { sessionId });
+  }
+
+  emitToSession(sessionId: string, message: string, payload: any) {
+    const socketId = this.reverseSessionMap.get(sessionId);
+    if (socketId) {
+      this.server.to(socketId).emit(message, payload);
+      this.cleanupSession(sessionId); // immediately remove session since it is one time use only
+    }
+  }
+
+  private cleanupSession(sessionId: string) {
+    const socketId = this.reverseSessionMap.get(sessionId);
+
+    if (socketId) {
+      this.sessionMap.delete(socketId);
+    }
+
+    this.reverseSessionMap.delete(sessionId);
   }
 }
